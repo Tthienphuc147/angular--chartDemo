79d1daf95baa33f4d6310c125a15486a2375c709
phuc <Ngoc Phuc@DESKTOP-VPNO4HN>
2020-03-24 11:09:48 +0700

    -- add chart

--- p1/src/app/app.component.ts
+++ p1/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'p1';
+  private chart: am4charts.XYChart;
+  constructor(private zone: NgZone){};
+
+  ngAfterViewInit(){
+    this.zone.runOutsideAngular(()=>{
+      const chart = am4core.create("line-chart",am4charts.XYChart);
+      const title = chart.titles.create();
+      title.text = "Product Sales by Area";
+
+      let data = [
+        {
+          "area": "Florida", "computers": 20, "cars": 50, "boats":60
+        },
+        {
+          "area": "Florida1", "computers": 30, "cars": 20, "boats":15
+        },
+        {
+          "area": "Florida2", "computers": 50, "cars": 40, "boats":65
+        },
+        {
+          "area": "Florida3", "computers": 10, "cars": 50, "boats":35
+        }
+      ];
+      chart.data = data;
+      let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.title.text = "Area";
+      categoryAxis.dataFields.category = "area";
+
+      let valueAxisY = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxisY.title.text = "Sales";
+      valueAxisY.renderer.minWidth =20;
+
+      let seriesNames = ['computers', 'cars', 'boats'];
+      for(let i=0; i<3; i++){
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.categoryX = "area";
+        series.dataFields.valueY = seriesNames[i];
+        series.name = seriesNames[i];
+        let bullet = series.bullets.push(new am4charts.CircleBullet());
+        bullet.circle.strokeWidth = 2;
+        bullet.circle.radius = 4;
+        bullet.tooltipText = "Area: {categoryX} \n Sales: {valueY} {name}";
+
+      }
+      chart.legend = new am4charts.Legend();
+      this.chart = chart;
+    })
+  }
+
+  ngOnDestroy(){
+    this.zone.runOutsideAngular(()=>{
+      if(this.chart){
+        this.chart.dispose();
+      }
+    })
+  }
 }
